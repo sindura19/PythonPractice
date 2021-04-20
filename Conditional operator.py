f37ab5177f129bf18406a37c4e676b17df2ec26c
sindura19 <sindura.pradhan@gmail.com>
2021-04-20 23:09:41 +0530

    Add files via upload
    20th April Python Practice

---
+++ Conditional operator.py
@@ -0,0 +1,24 @@
+#Write a program to check wheather a number is even or odd?
+num=int(input("Enter a number."))
+print(num," is Even") if num%2==0 else print(num," is Odd") 
+
+
+#Write a Program to Check if the person is eligible for voting or not?
+
+name=input("Enter person's name:")
+age=int(input("Enter person's age:"))
+print(name,"Eligible for voting") if age>18 else print(name,"not Eligible for voting")
+
+#write a program find the maximum of two number:
+n1,n2=map(int,input("Enter two values to compare").split(" "))
+print(n1,"is maximum") if n1>n2 else print(n2,"is maximum")
+
+#write a program find the maximum of two number:
+n1=int(input("Enter 1st number"))
+n2=int(input("Enter 1st number"))
+print(n1,"is maximum") if n1>n2 else print(n2,"is maximum")
+
+print("1st statement") print("1st statement") if True else print("else staement")
+#SyntaxError: invalid syntax
+
+
